bd8b7266d3c09bebc3ae0834d89ac580bf2bd635
Erick Valencia <evalencia0911@gmail.com>
2024-04-14 08:40:43 -0600

    svelte v1

---
+++ src/routes/films/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    import { FilmStore } from '../../film-store'
+
+    // can remove and add a hook
+    // let films = []
+
+    // const unsubscribe = FilmStore.subscribe(values => films = values)
+
+    // onDestroy(unsubscribe)
+</script>
+
+<!-- if statement -->
+<!-- {#if film.startsWith('F')}
+    <p>Starts with 'T''</p>
+{:else }
+    <p>Doesn't start with 'T'</p>
+{/if} -->
+
+<!-- define UL -->
+<ul>
+    {#each $FilmStore as film}
+        <li>
+            <a href="films/{film.id}">{film.name}
+        </li>
+
+    {/each}
+</ul>
---
+++ src/routes/films/[id]/+page.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { FilmStore } from '../../../film-store'
+
+
+    export let data;
+
+    let film = $FilmStore.find(film => film.id == data.id)
+    console.log(film)
+</script>
+
+<!-- if statement -->
+{#if film}
+    <p>{film.name}, directed by: {film.director}</p>
+{:else}
+    <p>No film was found with id {film.id}</p>
+{/if}
